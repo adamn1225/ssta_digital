dd7e85d1caa37885f213bea8c48ee17f4b0181c9
adamn1225 <anoah1225@gmail.com>
2024-06-16 08:41:34 -0500

    whats going on

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,8 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+    readonly CONTENTFUL_SPACE_ID: 'R-yEIO56irDrP83FOiyZ9wBbkQm7qSOfraBugQZ-AqY';
+    readonly CONTENTFUL_DELIVERY_TOKEN: 'R-yEIO56irDrP83FOiyZ9wBbkQm7qSOfraBugQZ-AqY';
+    readonly CONTENTFUL_PREVIEW_TOKEN: 'YkerdpSLY8NKuN2i3jecV87ZIF-iA_8fntlN-ct4XW0';
+}
